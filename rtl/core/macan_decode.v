57a0d01015de64310f895b7ec4df35359d843d3e
macro <makermuyi@163.com>
2022-03-24 23:34:25 +0800

    Add the pipeline fetch and decode file

---
+++ rtl/core/macan_decode.v
@@ -0,0 +1,62 @@
+//--------------------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.1
+//                         openedf.com
+//                     Copyright 2020-2021
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: Macro
+// Brief: RISC-V Instruction Decode file: understand waht instruction means
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+`include "macan_defines.v"
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+module macan_decode
+//--------------------------------------------------------------------------
+// Params
+//--------------------------------------------------------------------------
+#(
+
+)
+//--------------------------------------------------------------------------
+// Ports
+//--------------------------------------------------------------------------
+(
+    // Inputs
+    input clk,
+    input rst_n,
+
+    // Outputs
+);
+
+endmodule
+
+//--------------------------------------------------------------------------
