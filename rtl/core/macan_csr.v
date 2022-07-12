520d988d32cb4d4a05e4b5efbc4d00e5b3221929
macro <makermuyi@163.com>
2022-07-12 08:47:54 +0800

    Add the CRS memroy array

--- rtl/core/macan_csr.v
+++ rtl/core/macan_csr.v
@@ -27,7 +27,8 @@
 
 //--------------------------------------------------------------------------
 // Designer: Macro
-// Brief: RISC-V Instruction for Control and Status Instructions
+// Brief: RISC-V Instruction for Control and Status Instructions,the full
+//        set of CSR instructions that operate on these CSRs.
 // Change Log:
 //--------------------------------------------------------------------------
 
@@ -53,21 +54,44 @@ module macan_csr
     // Inputs
     input wire clk,
     input wire rst_n,
-    
-    // Inputs 
+
+    // Inputs and Outputs Address
     input wire [11:0] csr_addr,
     input wire [31:0] write_data,
     input wire        write_en,
 
     // Outputs
-    output reg [31:0] read_data,
-    output reg [31:0] ctrl_bus
+    output reg  [31:0] read_data,
+    output wire [31:0] ctrl_bus
 );
 
-always @(*) begin
-    if (!rst_n) begin
+// Control and Status Register Memory Model: RISC-V defines a separate
+// address space of 4096 Control and Status registers associated  with
+// each hart. width = 32, depth = 4096
+reg [31:0] csr_mem_reg[0:4095];
 
+// initialize memory and register
+initial begin
+    csr_mem_reg[`M_CSR_MVENDORID_ADDR] = 32'h12345678;
 end
 
+// read write data from csr memory
+always @(*) begin
+    if (!rst_n) begin
+        read_data <= 32'h0;
+        ctrl_bus <= 32'h0;
+    end else begin
+        if (write_en) begin
+            csr_mem_reg[csr_addr] <= write_data;
+        end else begin
+            read_data <= csr_mem_reg[csr_addr];
+        end
+    end
+end
+
+// output the control bus signal
+assign ctrl_bus = {csr_mem_reg[`M_CSR_MVENDORID_ADDR][3:0]};
+
 endmodule
+
 //--------------------------------------------------------------------------
